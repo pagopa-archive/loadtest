ce8c71bd2ace510ca8db1a5370f756abd555dc34
pasqualedevita <>
2021-06-02 23:59:11 +0200

    fix fiscalcodes

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -1,7 +1,7 @@
-import configData from '../config.json';
+var fiscalCodes = ["AAAAAA00A00A000A"];
 
 export function generateFakeFiscalCode() {
-  var items = configData.fiscalCodes;
+  var items = fiscalCodes;
   var item = items[Math.floor(Math.random() * items.length)];
 
   return item;

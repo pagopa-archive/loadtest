858f142041c1aef392a3beb54b890a2a726b9b97
Tommaso Stocchi <tommasostocchi@outlook.com>
2021-06-03 16:03:11 +0200

    -fix

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -1,21 +1,21 @@
 const configData=JSON.parse(open("../config.json"));
 
 export function generateFakeFiscalCode() {
-  fiscalCodes = configData.fiscalCodes;
+  var fiscalCodes = configData.fiscalCodes;
   var fiscalCode = fiscalCodes[Math.floor(Math.random() * items.length)];
 
   return fiscalCode;
 }
 
 export function generateFakAuthCode() {
-  authCodes = configData.authCodes;
+  var authCodes = configData.authCodes;
   var authCode = authCodes[Math.floor(Math.random() * items.length)];
 
   return authCode;
 }
 
 export function generateFakeSessionToken() {
-  sessionTokens = configData.sessionTokens;
+  var sessionTokens = configData.sessionTokens;
   var sessionToken = sessionTokens[Math.floor(Math.random() * items.length)];
 
   return sessionToken;
